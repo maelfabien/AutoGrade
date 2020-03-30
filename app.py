1635a3e4792fb8741bae535f6a541491071df867
Mael Fabien <mael.fabien@anasen.com>
2020-03-30 14:36:27 +0200

    Remove accents

--- app.py
+++ app.py
@@ -20,12 +20,48 @@ w3 = st.sidebar.text_input("Mots Q3")
 st.header("Notation automatique")
 st.write("Collez la réponse de l'élève dans la question appropriée")
 
+def rmv_acc(string_1):
+
+    string_1 = string_1.replace("ç", "c")
+    string_1 = string_1.replace("Ç", "C")
+    string_1 = string_1.replace("à", "a")
+    string_1 = string_1.replace("Ä", "A")
+    string_1 = string_1.replace("ä", "a")
+    string_1 = string_1.replace("À", "A")
+    string_1 = string_1.replace("Â", "A")
+    string_1 = string_1.replace("â", "a")
+    string_1 = string_1.replace("é", "e")
+    string_1 = string_1.replace("è", "e")
+    string_1 = string_1.replace("É", "E")
+    string_1 = string_1.replace("È", "E")
+    string_1 = string_1.replace("Ë", "E")
+    string_1 = string_1.replace("ë", "e")
+    string_1 = string_1.replace("Ê", "E")
+    string_1 = string_1.replace("ê", "e")
+    string_1 = string_1.replace("û", "u")
+    string_1 = string_1.replace("Û", "U")
+    string_1 = string_1.replace("ü", "u")
+    string_1 = string_1.replace("Ü", "U")
+    string_1 = string_1.replace("ï", "i")
+    string_1 = string_1.replace("Ï", "I")
+    string_1 = string_1.replace("î", "i")
+    string_1 = string_1.replace("Î", "I")
+    string_1 = string_1.replace("Ô", "O")
+    string_1 = string_1.replace("ô", "o")
+    string_1 = string_1.replace("Ö", "O")
+    string_1 = string_1.replace("ö", "o")
+    string_1 = string_1.replace("Ù", "U")
+    string_1 = string_1.replace("ù", "u")
+
+    return string_1
+
 def return_grade(text, list_words):
 	text = text.lower()
 
 	text = text.split()
 	text = ' '.join([stemmer.stem(t) for t in text])
 
+	text = ' '.join([rmv_acc(t) for t in text.split()])
 	if list_words != "":
 		list_words = list_words.split(", ")
 		list_words = [l.lower() for l in list_words]
@@ -35,6 +71,7 @@ def return_grade(text, list_words):
 		for word in list_words:
 
 			word = ' '.join([stemmer.stem(w) for w in word.split()])
+			word = rmv_acc(word)
 
 			if word in text:
 				count += 1
